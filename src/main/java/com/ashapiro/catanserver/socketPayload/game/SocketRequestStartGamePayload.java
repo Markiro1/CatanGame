fb75a65980e704ce8cb3784f3a29562f758ea29e
Markiro1 <65sumo@gmail.com>
2024-05-30 14:28:10 +0300

    - added new SocketRequest class SocketRequestStartGamePayload.java

--- src/main/java/com/ashapiro/catanserver/socketPayload/game/SocketRequestStartGamePayload.java
+++ src/main/java/com/ashapiro/catanserver/socketPayload/game/SocketRequestStartGamePayload.java
@@ -1,6 +1,13 @@
 package com.ashapiro.catanserver.socketPayload.game;
 
 import com.ashapiro.catanserver.socketPayload.SocketMessagePayload;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.util.List;
+
+@Getter @Setter
 public class SocketRequestStartGamePayload extends SocketMessagePayload {
+
+    private List<Integer> map;
 }
