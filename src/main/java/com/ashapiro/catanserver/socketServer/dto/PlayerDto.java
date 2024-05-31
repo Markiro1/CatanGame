21286e5fc28269b81968ddb9b401e2e07d868ee0
Markiro1 <65sumo@gmail.com>
2024-05-31 17:17:26 +0300

    - added ned class DTO PlayerDTO

---
+++ src/main/java/com/ashapiro/catanserver/socketServer/dto/PlayerDto.java
@@ -0,0 +1,12 @@
+package com.ashapiro.catanserver.socketServer.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class PlayerDto {
+    private Long id;
+
+    private String name;
+}
