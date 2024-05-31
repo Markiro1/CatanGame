92ff655caeaf76947bb6d5ce32b0426adf2b4f62
Markiro1 <65sumo@gmail.com>
2024-05-31 17:17:56 +0300

    - added new broadcast class SocketBroadcastUserReadyAndLoadPayload

---
+++ src/main/java/com/ashapiro/catanserver/socketServer/payload/broadcast/SocketBroadcastUserReadyAndLoadPayload.java
@@ -0,0 +1,18 @@
+package com.ashapiro.catanserver.socketServer.payload.broadcast;
+
+import com.ashapiro.catanserver.enums.EventType;
+import com.ashapiro.catanserver.socketServer.dto.PlayerDto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class SocketBroadcastUserReadyAndLoadPayload implements SocketBroadcastPayload{
+    private EventType eventType;
+
+    private PlayerDto playerDto;
+
+    private String message;
+}
