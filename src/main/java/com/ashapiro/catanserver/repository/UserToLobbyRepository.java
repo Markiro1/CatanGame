faaeee1b6fa9b3e4d19d248646e25261aaae0b87
Markiro1 <65sumo@gmail.com>
2024-05-31 17:15:08 +0300

    - deleted extra imports and method

--- src/main/java/com/ashapiro/catanserver/repository/UserToLobbyRepository.java
+++ src/main/java/com/ashapiro/catanserver/repository/UserToLobbyRepository.java
@@ -2,17 +2,10 @@ package com.ashapiro.catanserver.repository;
 
 import com.ashapiro.catanserver.entity.UserToLobby;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
-import java.util.Optional;
 
 public interface UserToLobbyRepository extends JpaRepository<UserToLobby, Long> {
 
     void deleteByUserId(Long userId);
-
-    @Query("select utl.lobby.id from UserToLobby utl where utl.user.id = :userId")
-    Long getLobbyIdByUserId(Long userId);
 
     Long findLobbyIdByUserToken(String token);
 }
