fbaf64873da7ef60a4fbb664322a53be1a87cc1f
Markiro1 <65sumo@gmail.com>
2024-05-31 17:18:40 +0300

    - added new class SocketRequestReadyAndLoadPayload.java

---
+++ src/main/java/com/ashapiro/catanserver/socketServer/payload/request/SocketRequestReadyAndLoadPayload.java
@@ -0,0 +1,6 @@
+package com.ashapiro.catanserver.socketServer.payload.request;
+
+import com.ashapiro.catanserver.socketServer.payload.SocketMessagePayload;
+
+public class SocketRequestReadyAndLoadPayload extends SocketMessagePayload {
+}
