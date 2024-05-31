0a999881db3adebd6a91edc64df14b733616fab3
Markiro1 <65sumo@gmail.com>
2024-05-31 17:18:26 +0300

    - added new class SocketHandlerImpl.java

---
+++ src/main/java/com/ashapiro/catanserver/socketServer/SocketHandlerImpl.java
@@ -0,0 +1,46 @@
+package com.ashapiro.catanserver.socketServer;
+
+import com.ashapiro.catanserver.socketServer.payload.SocketMessagePayload;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.net.Socket;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class SocketHandlerImpl implements SocketHandler{
+
+    private final SocketServiceImpl socketService;
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void onConnection(Socket clientSocket) {
+        log.info("New client connected: " + clientSocket.getInetAddress().getHostAddress());
+        SocketInfo socketInfo = new SocketInfo(clientSocket, null, null);
+        socketService.getSocketInfos().add(socketInfo);
+    }
+
+    @Override
+    public void onClose(Socket clientSocket) {
+        socketService.disconnectFromLobby(clientSocket);
+        try {
+            clientSocket.close();
+        } catch (IOException e) {
+            log.error("Error close the socket: " + clientSocket.getInetAddress().getHostAddress());
+        }
+    }
+
+    @Override
+    public <T extends SocketMessagePayload> void onMessage(Socket clientSocket, T message) {
+        switch (message.getEventType()) {
+            case REQUEST_CONNECT_TO_LOBBY -> socketService.connectToLobby(clientSocket, message);
+            case REQUEST_START_GAME -> socketService.startGame(clientSocket, message);
+            case REQUEST_READY_AND_LOAD -> socketService.updatePlayerReadyStatus(clientSocket);
+        }
+    }
+}
