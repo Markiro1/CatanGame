fe7825800533b4952b84b04c01067b34dd5bfc9d
Markiro1 <65sumo@gmail.com>
2024-05-31 17:08:55 +0300

    - delete extra imports - changed name of Lobby to LobbyEntity

--- src/main/java/com/ashapiro/catanserver/repository/LobbyRepository.java
+++ src/main/java/com/ashapiro/catanserver/repository/LobbyRepository.java
@@ -1,34 +1,33 @@
 package com.ashapiro.catanserver.repository;
 
 import com.ashapiro.catanserver.dto.lobby.AllLobbyDto;
-import com.ashapiro.catanserver.dto.lobby.LobbyDetailsDto;
-import com.ashapiro.catanserver.entity.Lobby;
-import com.ashapiro.catanserver.entity.User;
+import com.ashapiro.catanserver.entity.LobbyEntity;
+import com.ashapiro.catanserver.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface LobbyRepository extends JpaRepository<Lobby, Long> {
+public interface LobbyRepository extends JpaRepository<LobbyEntity, Long> {
 
     @Query("select new com.ashapiro.catanserver.dto.lobby.AllLobbyDto(" +
             "l.id, " +
             "l.name," +
             "count (u)" +
-            ") from Lobby l " +
+            ") from LobbyEntity l " +
             "join UserToLobby utl on utl.lobby.id = l.id " +
-            "join User u on utl.user.id = u.id " +
+            "join UserEntity u on utl.user.id = u.id " +
             "group by l.id")
     List<AllLobbyDto> findAllLobbies();
 
     @Query("select distinct l " +
-            "from Lobby l " +
+            "from LobbyEntity l " +
             "left join fetch UserToLobby utl on utl.lobby = l " +
-            "join User u on utl.user = u " +
+            "join UserEntity u on utl.user = u " +
             "where u = :user")
-    Optional<Lobby> findLobbyByUser(User user);
+    Optional<LobbyEntity> findLobbyByUser(UserEntity user);
 
-    @Query("select l from Lobby l left join fetch l.usersToLobby where l.id = :id")
-    Optional<Lobby> findLobbyByIdFetchUserToLobby(Long id);
+    @Query("select l from LobbyEntity l left join fetch l.usersToLobby where l.id = :id")
+    Optional<LobbyEntity> findLobbyByIdFetchUserToLobby(Long id);
 }
