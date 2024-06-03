b6234e4e9282cf15dfa43280b4891160d8872edb
Markiro1 <65sumo@gmail.com>
2024-06-03 18:19:02 +0300

    - removed extra methods

--- src/main/java/com/ashapiro/catanserver/socketServer/SocketService.java
+++ src/main/java/com/ashapiro/catanserver/socketServer/SocketService.java
@@ -7,11 +7,13 @@ import java.net.Socket;
 public interface SocketService {
     <T extends SocketMessagePayload> void connectToLobby(Socket clientSocket, T message);
 
-    void disconnectFromLobby(Socket clientSocket);
-
     <T extends SocketMessagePayload> void startGame(Socket clientSocket, T message);
 
-    void updatePlayerReadyStatus(Socket clientSocket);
+    <T extends SocketMessagePayload> void buildHouse(Socket clientSocket, T message);
+
+    void disconnectFromLobby(Socket clientSocket);
 
     void removeUserFromLobbyIfPresent(String token);
+
+    void updateUserReadyStatus(Socket clientSocket);
 }
