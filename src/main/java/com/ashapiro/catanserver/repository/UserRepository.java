8105d5b0a49a23d33a23144a642977447142749e
Markiro1 <65sumo@gmail.com>
2024-06-03 18:19:58 +0300

    - removed extra methods

--- src/main/java/com/ashapiro/catanserver/repository/UserRepository.java
+++ src/main/java/com/ashapiro/catanserver/repository/UserRepository.java
@@ -1,12 +1,10 @@
 package com.ashapiro.catanserver.repository;
 
-import com.ashapiro.catanserver.dto.user.SimpleUserDto;
 import com.ashapiro.catanserver.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
@@ -20,12 +18,4 @@ public interface UserRepository extends JpaRepository<UserEntity, Long> {
     Optional<String> findLoginByToken(@Param("token") String token);
 
     boolean existsByLogin(String login);
-
-    @Query("select u.token from UserEntity u " +
-            "join UserToLobby utl on utl.user.id = u.id " +
-            "where utl.lobby.id = :lobbyId")
-    List<String> retrieveTokensByLobbyId(Long lobbyId);
-
-    @Query("select new com.ashapiro.catanserver.dto.user.SimpleUserDto(u.id, u.login, u.username) from UserEntity u where u.token = :token")
-    Optional<SimpleUserDto> findSimpleUserByToken(String token);
 }
