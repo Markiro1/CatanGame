ca2dd7b48931e10aeb5d3e2ba88630973047cbb2
Markiro1 <65sumo@gmail.com>
2024-06-03 18:16:47 +0300

    - removed extra methods

--- src/main/java/com/ashapiro/catanserver/config/SecurityConfig.java
+++ src/main/java/com/ashapiro/catanserver/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.ashapiro.catanserver.config;
 
 import com.ashapiro.catanserver.util.JwtRequestFilter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
