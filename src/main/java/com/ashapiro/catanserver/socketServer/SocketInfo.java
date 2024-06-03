6c0447a1a1528a3dc3e127a91af450419a0414e4
Markiro1 <65sumo@gmail.com>
2024-06-03 18:17:40 +0300

    - removed @ToString

--- src/main/java/com/ashapiro/catanserver/socketServer/SocketInfo.java
+++ src/main/java/com/ashapiro/catanserver/socketServer/SocketInfo.java
@@ -4,13 +4,11 @@ import com.ashapiro.catanserver.socketServer.util.Lobby;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 
 import java.net.Socket;
 
 @Getter @Setter
 @AllArgsConstructor
-@ToString
 public class SocketInfo {
 
     private Socket socket;
