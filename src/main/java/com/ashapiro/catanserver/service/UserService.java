2518b2b9f8f2ce84da2ac28dae1f13b142cc512d
Markiro1 <65sumo@gmail.com>
2024-06-03 18:12:42 +0300

    - dto prefix changed to uppercase - remove extra methods

--- src/main/java/com/ashapiro/catanserver/service/UserService.java
+++ src/main/java/com/ashapiro/catanserver/service/UserService.java
@@ -1,25 +1,16 @@
 package com.ashapiro.catanserver.service;
 
-import com.ashapiro.catanserver.dto.auth.RegisterDto;
-import com.ashapiro.catanserver.dto.user.SimpleUserDto;
+import com.ashapiro.catanserver.dto.auth.RegisterDTO;
+import com.ashapiro.catanserver.dto.user.SimpleUserDTO;
 import com.ashapiro.catanserver.entity.UserEntity;
-import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface UserService {
 
-    SimpleUserDto save(RegisterDto registerDto);
+    SimpleUserDTO save(RegisterDTO registerDto);
 
     void updateUserTokenByLogin(String login, String token);
 
-    @Query("select u from UserEntity u left join fetch u.userToLobby")
-    Optional<UserEntity> findUserByLogin(String login);
-
-    Optional<UserEntity> findUserByToken(String token);
-
-    List<String> retrieveTokensByLobbyId(Long lobbyId);
-
-    Optional<SimpleUserDto> findSimpleUserByToken(String token);
+    Optional<UserEntity> findUserEntityByToken(String token);
 }
