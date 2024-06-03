71fab20fcec62bee28474c2c6d554f4b2c67c67f
Markiro1 <65sumo@gmail.com>
2024-06-03 18:16:28 +0300

    - added REQUEST_BUILD_HOUSE in MessageRegistry.java

--- src/main/java/com/ashapiro/catanserver/socketServer/util/MessageRegistry.java
+++ src/main/java/com/ashapiro/catanserver/socketServer/util/MessageRegistry.java
@@ -2,9 +2,10 @@ package com.ashapiro.catanserver.socketServer.util;
 
 import com.ashapiro.catanserver.enums.EventType;
 import com.ashapiro.catanserver.socketServer.payload.SocketMessagePayload;
+import com.ashapiro.catanserver.socketServer.payload.request.SocketRequestBuildHousePayload;
 import com.ashapiro.catanserver.socketServer.payload.request.SocketRequestReadyAndLoadPayload;
 import com.ashapiro.catanserver.socketServer.payload.request.SocketRequestStartGamePayload;
-import com.ashapiro.catanserver.socketServer.payload.request.SocketRequestConnectToLobbyPayload;
+import com.ashapiro.catanserver.socketServer.payload.request.SocketRequestConnectPayload;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -13,9 +14,10 @@ public class MessageRegistry {
     private static final Map<EventType, Class<? extends SocketMessagePayload>> registry = new HashMap<>();
 
     static {
-        registry.put(EventType.REQUEST_CONNECT_TO_LOBBY, SocketRequestConnectToLobbyPayload.class);
+        registry.put(EventType.REQUEST_CONNECT, SocketRequestConnectPayload.class);
         registry.put(EventType.REQUEST_START_GAME, SocketRequestStartGamePayload.class);
         registry.put(EventType.REQUEST_READY_AND_LOAD, SocketRequestReadyAndLoadPayload.class);
+        registry.put(EventType.REQUEST_BUILD_HOUSE, SocketRequestBuildHousePayload.class);
     }
 
     public static <T extends SocketMessagePayload> Class<T> getMessageClass(EventType eventType) {
