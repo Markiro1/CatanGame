e4a7f84960d8249316353ae4408bef43dad7fda0
Markiro1 <65sumo@gmail.com>
2024-06-03 18:14:21 +0300

    - added new methods sendMessageUserTurn(), checkUsersReady() - added new logic

--- src/main/java/com/ashapiro/catanserver/game/CatanGame.java
+++ src/main/java/com/ashapiro/catanserver/game/CatanGame.java
@@ -1,22 +1,32 @@
 package com.ashapiro.catanserver.game;
 
+import com.ashapiro.catanserver.enums.EventType;
 import com.ashapiro.catanserver.enums.GameState;
 import com.ashapiro.catanserver.game.model.Edge;
 import com.ashapiro.catanserver.game.model.Hex;
-import com.ashapiro.catanserver.game.model.Player;
+import com.ashapiro.catanserver.game.model.User;
 import com.ashapiro.catanserver.game.model.Vertex;
+import com.ashapiro.catanserver.socketServer.payload.broadcast.SocketBroadcastPayload;
+import com.ashapiro.catanserver.socketServer.payload.broadcast.SocketBroadcastUserTurnPayload;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.List;
 
-@Getter @Setter
+@Getter
+@Setter
+@Slf4j
 public class CatanGame {
 
     private GameState gameState;
 
-    private List<Player> players;
+    private List<User> users;
 
     private List<Hex> hexes;
 
@@ -24,8 +34,10 @@ public class CatanGame {
 
     private List<Vertex> vertices;
 
-    public CatanGame(List<Player> players, List<Hex> hexes, List<Edge> edges, List<Vertex> vertices) {
-        this.players = players;
+    private static final int TURN_DURATION = 60000;
+
+    public CatanGame(List<User> users, List<Hex> hexes, List<Edge> edges, List<Vertex> vertices) {
+        this.users = new ArrayList<>(users);
         this.hexes = hexes;
         this.edges = edges;
         this.vertices = vertices;
@@ -36,22 +48,58 @@ public class CatanGame {
     }
 
     private class GameManager implements Runnable {
+
         @Override
         @SneakyThrows
         public void run() {
-            // todo: game logic
-            Thread.sleep(3000);
-            long count = players.stream()
-                    .filter(p -> p.getIsReady())
-                    .count();
-            if (count == players.size()) {
-                gameState = GameState.STARTED;
-            }
-            do {
-                for (Player p : players) {
-
+            for (int i = 0; i < 15; i++) {
+                if (checkUsersReady()) {
+                    gameState = GameState.STARTED;
+                    break;
                 }
-            } while (true ); // todo: add some logic
+                System.out.println("check user ready " + i);
+                Thread.sleep(1000);
+            }
+
+            if (!checkUsersReady()) {
+                users.stream()
+                        .filter(user -> !user.getIsReady())
+                        .forEach(user -> {
+                            try {
+                                user.getSocket().close();
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
+                            }
+                        });
+                users.removeIf(user -> !user.getIsReady());
+            }
+
+            do {
+                for (User user : users) {
+                    if (!user.getSocket().isClosed()) {
+                        sendMessageUserTurn(user, EventType.BROADCAST_USER_TURN);
+                    }
+                    Thread.sleep(TURN_DURATION);
+                }
+            } while (true);
+        }
+
+        private void sendMessageUserTurn(User user, EventType eventType) throws IOException {
+            for (User u : users) {
+                SocketBroadcastPayload broadcast = new SocketBroadcastUserTurnPayload(
+                        eventType,
+                        user.getId()
+                );
+                ObjectMapper objectMapper = new ObjectMapper();
+                String message = objectMapper.writeValueAsString(broadcast);
+                PrintWriter printWriter = new PrintWriter(u.getSocket().getOutputStream());
+                printWriter.println(message);
+                printWriter.flush();
+            }
+        }
+
+        private boolean checkUsersReady() {
+            return users.stream().allMatch(User::getIsReady);
         }
     }
 }
