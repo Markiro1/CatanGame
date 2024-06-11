65f2a7c091ae231c253fa310c904175c0eeb01bf
Markiro1 <65sumo@gmail.com>
2024-06-11 16:33:07 +0300

    - change logic of method startGame

--- src/main/java/com/ashapiro/catanserver/socketServer/util/Lobby.java
+++ src/main/java/com/ashapiro/catanserver/socketServer/util/Lobby.java
@@ -2,8 +2,7 @@ package com.ashapiro.catanserver.socketServer.util;
 
 import com.ashapiro.catanserver.game.CatanGame;
 import com.ashapiro.catanserver.game.MapGenerator;
-import com.ashapiro.catanserver.game.dto.HexDTO;
-import com.ashapiro.catanserver.game.model.User;
+import com.ashapiro.catanserver.game.User;
 import lombok.Getter;
 import lombok.ToString;
 
@@ -28,7 +27,7 @@ public class Lobby {
         this.userMap = new ConcurrentHashMap<>();
     }
 
-    public List<HexDTO> startGame(List<Integer> map) {
+    public int startGame(List<Integer> map) {
         List<User> users = userMap.entrySet()
                 .stream()
                 .map(Map.Entry::getValue)
@@ -43,9 +42,6 @@ public class Lobby {
                 mapGenerator.getVertices()
         );
 
-        return mapGenerator.getHexes()
-                .stream()
-                .map(hex -> new HexDTO(hex.getId(), hex.getType(), hex.getNumberToken()))
-                .collect(Collectors.toList());
+        return mapGenerator.getSeed();
     }
 }
