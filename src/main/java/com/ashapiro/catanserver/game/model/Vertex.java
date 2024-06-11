7cd574e429ac7970d525512952af7c78f5b2361b
Markiro1 <65sumo@gmail.com>
2024-06-11 16:37:40 +0300

    - added new field to vertex class

--- src/main/java/com/ashapiro/catanserver/game/model/Vertex.java
+++ src/main/java/com/ashapiro/catanserver/game/model/Vertex.java
@@ -1,5 +1,7 @@
 package com.ashapiro.catanserver.game.model;
 
+import com.ashapiro.catanserver.game.User;
+import com.ashapiro.catanserver.game.enums.VertexBuildingType;
 import lombok.*;
 
 import java.util.ArrayList;
@@ -10,9 +12,14 @@ import java.util.List;
 @NoArgsConstructor
 @ToString
 public class Vertex {
+
     private Integer id;
 
     private List<Hex> hexes = new ArrayList<>();
 
-    private List<Edge> edges = new ArrayList<>();
+    private List<Edge> neighbourEdges = new ArrayList<>();
+
+    private VertexBuildingType type;
+
+    private User user;
 }
