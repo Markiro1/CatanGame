0d2486c21b447ccdd722c12d39d734aee8254f84
Markiro1 <65sumo@gmail.com>
2024-06-11 16:37:08 +0300

    - deleted extra exception class

--- src/main/java/com/ashapiro/catanserver/exceptions/socket/StartGameException.java
+++
@@ -1,7 +0,0 @@
-package com.ashapiro.catanserver.exceptions.socket;
-
-public class StartGameException extends RuntimeException {
-    public StartGameException(String username) {
-        super(String.format("User %s cannot start game, because he is not host"));
-    }
-}
