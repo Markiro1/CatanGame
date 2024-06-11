afe652c7b06d9800a3af770c4d7b557f03939602
Markiro1 <65sumo@gmail.com>
2024-06-11 16:32:33 +0300

    - added new game states

--- src/main/java/com/ashapiro/catanserver/enums/GameState.java
+++
@@ -1,6 +0,0 @@
-package com.ashapiro.catanserver.enums;
-
-public enum GameState {
-    WAITING,
-    STARTED
-}
---
+++ src/main/java/com/ashapiro/catanserver/game/enums/GameState.java
@@ -0,0 +1,9 @@
+package com.ashapiro.catanserver.game.enums;
+
+public enum GameState {
+    WAITING,
+    STARTED,
+    PREPARATION_BUILD_SETTLEMENTS,
+    FINISHED,
+    PREPARATION_BUILD_ROADS
+}
