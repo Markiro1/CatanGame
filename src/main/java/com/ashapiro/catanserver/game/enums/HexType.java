8f445d942b1d10bd2bd8e204e16dfdc1210e1595
Markiro1 <65sumo@gmail.com>
2024-06-11 16:32:48 +0300

    - added new method to take resource base on hex type

--- src/main/java/com/ashapiro/catanserver/game/enums/HexType.java
+++ src/main/java/com/ashapiro/catanserver/game/enums/HexType.java
@@ -6,5 +6,17 @@ public enum HexType {
     FOREST,
     HILL,
     MOUNTAIN,
-    DESERT
+    DESERT;
+
+    public Resource getResource() {
+        switch (this) {
+            case PASTURE: return Resource.WOOL;
+            case FIELD: return Resource.GRAIN;
+            case FOREST: return Resource.LUMBER;
+            case HILL: return Resource.BRICK;
+            case MOUNTAIN: return Resource.ORE;
+            case DESERT: return null;
+            default: throw new IllegalArgumentException("Unknown hex type: " + this);
+        }
+    }
 }
