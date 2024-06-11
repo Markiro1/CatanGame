736ee4f13d7af705498702a4447f9ac84e20fa9f
Markiro1 <65sumo@gmail.com>
2024-06-11 16:34:27 +0300

    - added new handler of message

--- src/main/java/com/ashapiro/catanserver/socketServer/SocketHandlerImpl.java
+++ src/main/java/com/ashapiro/catanserver/socketServer/SocketHandlerImpl.java
@@ -1,6 +1,10 @@
 package com.ashapiro.catanserver.socketServer;
 
 import com.ashapiro.catanserver.socketServer.payload.SocketMessagePayload;
+import com.ashapiro.catanserver.socketServer.payload.request.SocketRequestBuildPayload;
+import com.ashapiro.catanserver.socketServer.payload.request.SocketRequestConnectPayload;
+import com.ashapiro.catanserver.socketServer.payload.request.SocketRequestStartGamePayload;
+import com.ashapiro.catanserver.socketServer.payload.request.SocketRequestTradeResourcePayload;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -11,7 +15,7 @@ import java.net.Socket;
 @Component
 @RequiredArgsConstructor
 @Slf4j
-public class SocketHandlerImpl implements SocketHandler{
+public class SocketHandlerImpl implements SocketHandler {
 
     private final SocketServiceImpl socketService;
 
@@ -27,6 +31,7 @@ public class SocketHandlerImpl implements SocketHandler{
         socketService.disconnectFromLobby(clientSocket);
         try {
             clientSocket.close();
+            socketService.getSocketInfos().remove(clientSocket);
         } catch (IOException e) {
             log.error("Error close the socket: " + clientSocket.getInetAddress().getHostAddress());
         }
@@ -35,10 +40,55 @@ public class SocketHandlerImpl implements SocketHandler{
     @Override
     public <T extends SocketMessagePayload> void onMessage(Socket clientSocket, T message) {
         switch (message.getEventType()) {
-            case REQUEST_CONNECT -> socketService.connectToLobby(clientSocket, message);
-            case REQUEST_START_GAME -> socketService.startGame(clientSocket, message);
-            case REQUEST_READY_AND_LOAD -> socketService.updateUserReadyStatus(clientSocket);
-            case REQUEST_BUILD_HOUSE -> socketService.buildHouse(clientSocket, message);
+            case REQUEST_CONNECT -> handleConnect(clientSocket, message);
+            case REQUEST_START_GAME -> handleStartGame(clientSocket, message);
+            case REQUEST_READY_AND_LOAD, REQUEST_USER_TURN_READY -> socketService.updateUserReadyStatus(clientSocket);
+            case REQUEST_BUILD_SETTLEMENT -> handleBuildSettlement(clientSocket, message);
+            case REQUEST_BUILD_ROAD -> handleBuildRoad(clientSocket, message);
+            case REQUEST_BUILD_CITY -> handleBuildCity(clientSocket, message);
+            case REQUEST_TRADE_RESOURCE -> handleTradeResource(clientSocket, message);
+        }
+    }
+
+    private <T extends SocketMessagePayload> void handleTradeResource(Socket clientSocket, T message) {
+        if (message instanceof SocketRequestTradeResourcePayload) {
+            SocketRequestTradeResourcePayload socketMessage = (SocketRequestTradeResourcePayload) message;
+            socketService.tradeResource(clientSocket, socketMessage);
+        }
+    }
+
+    private <T extends SocketMessagePayload> void handleConnect(Socket clientSocket, T message) {
+        if (message instanceof SocketRequestConnectPayload) {
+            SocketRequestConnectPayload socketMessage = (SocketRequestConnectPayload) message;
+            socketService.connectToLobby(clientSocket, socketMessage);
+        }
+    }
+
+    private <T extends SocketMessagePayload> void handleStartGame(Socket clientSocket, T message) {
+        if (message instanceof SocketRequestStartGamePayload) {
+            SocketRequestStartGamePayload socketMessage = (SocketRequestStartGamePayload) message;
+            socketService.startGame(clientSocket, socketMessage);
+        }
+    }
+
+    private <T extends SocketMessagePayload> void handleBuildSettlement(Socket clientSocket, T message) {
+        if (message instanceof SocketRequestBuildPayload) {
+            SocketRequestBuildPayload socketMessage = (SocketRequestBuildPayload) message;
+            socketService.buildSettlement(clientSocket, socketMessage);
+        }
+    }
+
+    private <T extends SocketMessagePayload> void handleBuildRoad(Socket clientSocket, T message) {
+        if (message instanceof SocketRequestBuildPayload) {
+            SocketRequestBuildPayload socketMessage = (SocketRequestBuildPayload) message;
+            socketService.buildRoad(clientSocket, socketMessage);
+        }
+    }
+
+    private <T extends SocketMessagePayload> void handleBuildCity(Socket clientSocket, T message) {
+        if (message instanceof SocketRequestBuildPayload) {
+            SocketRequestBuildPayload socketMessage = (SocketRequestBuildPayload) message;
+            socketService.buildCity(clientSocket, socketMessage);
         }
     }
 }
