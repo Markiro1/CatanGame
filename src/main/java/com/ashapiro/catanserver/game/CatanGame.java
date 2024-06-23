b607fbfb1f475f249042637f5e25a5171a7e086a
Markiro1 <65sumo@gmail.com>
2024-06-23 14:41:34 +0300

    - added new field for CatanGame CLASS

--- src/main/java/com/ashapiro/catanserver/game/CatanGame.java
+++ src/main/java/com/ashapiro/catanserver/game/CatanGame.java
@@ -5,11 +5,13 @@ import com.ashapiro.catanserver.game.enums.EdgeBuildingType;
 import com.ashapiro.catanserver.game.enums.GameState;
 import com.ashapiro.catanserver.game.enums.Resource;
 import com.ashapiro.catanserver.game.enums.VertexBuildingType;
-import com.ashapiro.catanserver.game.model.Edge;
-import com.ashapiro.catanserver.game.model.Hex;
-import com.ashapiro.catanserver.game.model.Vertex;
+import com.ashapiro.catanserver.game.model.*;
+import com.ashapiro.catanserver.game.service.BankService;
+import com.ashapiro.catanserver.game.service.BankServiceImpl;
+import com.ashapiro.catanserver.game.service.GameService;
+import com.ashapiro.catanserver.game.service.GameServiceImpl;
 import com.ashapiro.catanserver.socketServer.SocketMessageSender;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ashapiro.catanserver.socketServer.SocketMessageSenderImpl;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
@@ -21,6 +23,9 @@ import java.util.concurrent.ThreadLocalRandom;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 
 @Getter
 @Setter
@@ -37,7 +42,15 @@ public class CatanGame extends Thread {
 
     private List<Vertex> vertices;
 
+    private int seed;
+
     private Bank bank;
+
+    private Lobby lobby;
+
+    private GameService gameService;
+
+    private BankService bankService;
 
     private SocketMessageSender socketMessageSender;
 
@@ -45,14 +58,18 @@ public class CatanGame extends Thread {
 
     private Integer numOfTurn = 1;
 
-    public CatanGame(List<User> users, List<Hex> hexes, List<Edge> edges, List<Vertex> vertices) {
+    public CatanGame(List<User> users, List<Hex> hexes, List<Edge> edges, List<Vertex> vertices, int seed, Lobby lobby) {
         this.users = new ArrayList<>(users);
-        this.bank = new BankImpl();
+        this.bank = new Bank();
         this.hexes = hexes;
         this.edges = edges;
         this.vertices = vertices;
-        gameState = GameState.WAITING;
-        socketMessageSender = new SocketMessageSender(new ObjectMapper());
+        this.seed = seed;
+        this.lobby = lobby;
+        this.gameState = GameState.WAITING;
+        this.gameService = GameServiceImpl.getInstance();
+        this.bankService = BankServiceImpl.getInstance();
+        this.socketMessageSender = SocketMessageSenderImpl.getInstance();
         start();
     }
 
@@ -70,20 +87,25 @@ public class CatanGame extends Thread {
 
     private void gameLoop() throws InterruptedException {
         log.info("STARTED MAIN GAME LOOP AFTER GAME PREPARATION");
-        List<Socket> sockets = getSocketsFromUsers();
+        List<Socket> sockets = getUsersSocket();
         while (!gameState.equals(GameState.FINISHED)) {
             for (numOfTurn = 1; numOfTurn <= 1000; numOfTurn++) {
                 for (User currentUser : users) {
                     executeTurn(sockets, currentUser);
+                    if (gameState.equals(GameState.FINISHED)) {
+                        break;
+                    }
+                }
+                if (gameState.equals(GameState.FINISHED)) {
+                    break;
                 }
             }
-            determineWinner(sockets);
         }
     }
 
     private void gamePreparation() {
         gameState = GameState.PREPARATION_BUILD_SETTLEMENTS;
-        List<Socket> sockets = getSocketsFromUsers();
+        List<Socket> sockets = getUsersSocket();
         for (numOfTurn = 1; numOfTurn <= 2; numOfTurn++) {
             executePreparationTurnForUsers(
                     sockets,
@@ -105,15 +127,103 @@ public class CatanGame extends Thread {
             );
         }
 
-        gameState = GameState.STARTED;
+        gameState = GameState.USER_TURN;
     }
 
-    private void executeTurn(List<Socket> sockets, User currentUser) throws InterruptedException {
+    private void executeTurn(List<Socket> sockets, User currentUser) {
         currentUser.setIsReady(false);
-        startTurn(sockets, currentUser, numOfTurn, EventType.BROADCAST_USER_TURN);
-        int resultOfRoll = diceRoll(currentUser, sockets);
-        supplyResourceToUser(currentUser, resultOfRoll);
+        gameState = GameState.PREPARING_USER_TURN;
+        startTurn(sockets, currentUser, numOfTurn, EventType.BROADCAST_PREPARE_USER_TURN);
+        int resultOfRoll = gameService.diceRoll(currentUser, sockets);
+        if (resultOfRoll == 7) {
+            gameState = GameState.ROBBERY;
+            socketMessageSender.broadcastStartRobbery(sockets);
+            randomStealResources(sockets);
+            boolean isDidAction = waitForUserRobbery(currentUser);
+            if (!isDidAction) {
+                robberPlacement(currentUser);
+            }
+        } else {
+            for (int i = users.indexOf(currentUser); i < users.size() + users.indexOf(currentUser); i++) {
+                supplyResourceToUser(users.get(i % users.size()), resultOfRoll);
+            }
+        }
+        if (checkForVictory(sockets, currentUser)) {
+            return;
+        }
+        gameState = GameState.USER_TURN;
+        socketMessageSender.broadcastUserTurn(sockets, EventType.BROADCAST_USER_TURN);
         waitForTurnEnd(currentUser);
+    }
+
+    private void startTurn(List<Socket> sockets, User currentUser, Integer numOfTurn, EventType eventType) {
+        socketMessageSender.broadcastPrepareUserTurn(sockets, currentUser.getId(), numOfTurn, eventType);
+        currentUser.setHisTurn(true);
+        currentUser.setIsReady(false);
+    }
+
+    private void randomStealResources(List<Socket> sockets) {
+        for (User user : users) {
+            long countOfResources = user.getResourceInventory().values().stream()
+                    .mapToInt(Integer::intValue)
+                    .sum();
+            if (countOfResources > 7) {
+                List<Resource> resources = user.getResourceInventory().entrySet().stream()
+                        .flatMap(entry -> Stream.generate(entry::getKey).limit(entry.getValue()))
+                        .collect(toList());
+                List<Resource> stolenResources = new ArrayList<>();
+                Collections.shuffle(resources);
+                for (int i = 0; i <= resources.size() / 2; i++) {
+                    Resource stolenResource = resources.get(i);
+                    stolenResources.add(stolenResource);
+                    user.retrieveResourceFromInventory(stolenResource, 1);
+                    bank.addResourceToStorage(stolenResource, 1);
+                }
+                socketMessageSender.broadcastRobberRobbery(sockets, user.getId(), stolenResources);
+            }
+        }
+    }
+
+    private void robberPlacement(User currentUser) {
+        if (!currentUser.isHisTurn() || !gameState.equals(GameState.ROBBERY)) {
+            return;
+        }
+        Hex robbedHex = hexes.stream().filter(Hex::isOccupiedByRobber)
+                .findFirst()
+                .orElse(null);
+
+        if (robbedHex == null) {
+            return;
+        }
+
+        List<User> usersInRobbedHex = robbedHex.getEdges().values().stream()
+                .filter(edge -> edge.getUser() != null && edge.getUser() != currentUser)
+                .map(Edge::getUser)
+                .collect(toList());
+
+        if (!usersInRobbedHex.isEmpty()) {
+            Random random = new Random();
+            Long victimId = usersInRobbedHex.get(random.nextInt(usersInRobbedHex.size())).getId();
+            gameService.userRobbery(this, currentUser, victimId, robbedHex.getId());
+        }
+    }
+
+    private void supplyResourceToUser(User user, int resultOfRoll) {
+        Set<Hex> userHexes = getUserHexes(user);
+        List<Resource> resources = collectResource(userHexes, resultOfRoll, user);
+        List<Socket> sockets = getUsersSocket();
+        bankService.supplyResourceToUser(bank, sockets, user, resources);
+    }
+
+
+    private boolean checkForVictory(List<Socket> sockets, User currentUser) {
+        int victoryPoints = currentUser.getUserVictoryPoints(vertices);
+        if (victoryPoints >= 10 && currentUser.isHisTurn()) {
+            gameState = GameState.FINISHED;
+            socketMessageSender.broadcastUserWin(sockets, currentUser.getId());
+            return true;
+        }
+        return false;
     }
 
     private void waitForTurnEnd(User currentUser) {
@@ -128,43 +238,23 @@ public class CatanGame extends Thread {
         }
     }
 
-    private void determineWinner(List<Socket> sockets) {
-        if (!users.isEmpty()) {
-            User randomUser = getRandomUser();
-            if (randomUser != null) {
-                socketMessageSender.broadcastUserWin(sockets, randomUser.getId());
-                gameState = GameState.FINISHED;
-            }
-        }
-    }
-
-    private User getRandomUser() {
-        Random random = new Random();
-        int randomIndex = random.nextInt(users.size());
-        return users.get(randomIndex);
-    }
-
-    private void supplyResourceToUser(User user, int resultOfRoll) {
-        Set<Hex> userHexes =  getUserHexes(user);
-        List<Resource> resources = collectResource(userHexes, resultOfRoll);
-        List<Socket> sockets = getSocketsFromUsers();
-        bank.supplyResourceToUser(socketMessageSender, sockets, user, resources);
-    }
-
-    private List<Resource> collectResource(Set<Hex> userHexes, int resultOfRoll) {
+    private List<Resource> collectResource(Set<Hex> userHexes, int resultOfRoll, User user) {
         List<Resource> resources = new ArrayList<>();
         for (Hex hex : userHexes) {
             if (hex.getNumberToken() == resultOfRoll) {
-                resources.addAll(getHexResources(hex));
+                resources.addAll(getHexResources(hex, user));
             }
         }
         return resources;
     }
 
-    private Collection<Resource> getHexResources(Hex hex) {
+    private Collection<Resource> getHexResources(Hex hex, User user) {
         List<Resource> resources = new ArrayList<>();
         Resource hexResource = hex.getType().getResource();
         for (Vertex vertex : hex.getVertices().values()) {
+            if (vertex.getUser() == null || !vertex.getUser().equals(user)) {
+                continue;
+            }
             if (vertex.getType().equals(VertexBuildingType.SETTLEMENT)) {
                 resources.add(hexResource);
             } else if (vertex.getType().equals(VertexBuildingType.CITY)) {
@@ -183,16 +273,6 @@ public class CatanGame extends Thread {
                 .collect(Collectors.toSet());
     }
 
-
-    private int diceRoll(User user, List<Socket> sockets) {
-        Random random = new Random();
-        int firstRandomNumber = random.nextInt(6) + 1;
-        int secondRandomNumber = random.nextInt(6) + 1;
-        int resultOfRoll = firstRandomNumber + secondRandomNumber;
-        socketMessageSender.broadcastUserDiceRoll(sockets, user, firstRandomNumber, secondRandomNumber);
-        return resultOfRoll;
-    }
-
     private void executePreparationTurnForUsers(
             List<Socket> sockets,
             Integer numOfTurn,
@@ -209,22 +289,37 @@ public class CatanGame extends Thread {
         }
     }
 
-
     private boolean waitForUserAction(User currentUser, Function<User, List<?>> getUserElements) {
         long turnEndTime = System.currentTimeMillis() + TURN_DURATION;
         while (System.currentTimeMillis() < turnEndTime || !currentUser.getIsReady()) {
             if (getUserElements.apply(currentUser).size() >= numOfTurn) {
                 return false;
             }
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
         currentUser.setHisTurn(false);
+        currentUser.setIsReady(false);
         return true;
     }
 
-    private void startTurn(List<Socket> sockets, User currentUser, Integer numOfTurn, EventType eventType) {
-        socketMessageSender.broadcastUserTurn(sockets, currentUser.getId(), numOfTurn, eventType);
-        currentUser.setHisTurn(true);
+    private boolean waitForUserRobbery(User currentUser) {
+        long turnEndTime = System.currentTimeMillis() + TURN_DURATION;
+        while (!currentUser.getIsReady()) {
+            if (System.currentTimeMillis() >= turnEndTime) {
+                return false;
+            }
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
         currentUser.setIsReady(false);
+        return true;
     }
 
     private void assignRandomSettlementToUser(User user) {
@@ -233,7 +328,7 @@ public class CatanGame extends Thread {
             randomVertex.setType(VertexBuildingType.SETTLEMENT);
             randomVertex.setUser(user);
             user.setHisTurn(false);
-            socketMessageSender.broadcastUserBuildSettlements(getSocketsFromUsers(), user, randomVertex.getId());
+            socketMessageSender.broadcastUserBuildSettlements(getUsersSocket(), user, randomVertex.getId());
         }
     }
 
@@ -244,7 +339,7 @@ public class CatanGame extends Thread {
             randomEdge.setType(EdgeBuildingType.ROAD);
             randomEdge.setUser(user);
             user.setHisTurn(false);
-            socketMessageSender.broadcastUserBuildRoads(getSocketsFromUsers(), user, randomEdge.getId());
+            socketMessageSender.broadcastUserBuildRoads(getUsersSocket(), user, randomEdge.getId());
         }
     }
 
@@ -302,11 +397,17 @@ public class CatanGame extends Thread {
         });
     }
 
-    private List<Socket> getSocketsFromUsers() {
+    public List<Socket> getUsersSocket() {
         return users.stream()
                 .map(User::getSocket)
                 .filter(socket -> !socket.isClosed())
                 .toList();
     }
 
+    public User getUserById(Long userId) {
+        return users.stream()
+                .filter(user -> user.getId().equals(userId))
+                .findFirst()
+                .orElseThrow(() -> new NoSuchElementException("User not found with id: " + userId));
+    }
 }
