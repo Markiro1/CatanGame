4d9d6d039f2a972d6c9c9b667318fee7d58c3b17
Markiro1 <65sumo@gmail.com>
2024-06-23 14:52:01 +0300

    - removed extra classes

--- src/main/java/com/ashapiro/catanserver/socketServer/payload/request/SocketRequestReadyAndLoadPayload.java
+++
@@ -1,6 +0,0 @@
-package com.ashapiro.catanserver.socketServer.payload.request;
-
-import com.ashapiro.catanserver.socketServer.payload.SocketMessagePayload;
-
-public class SocketRequestReadyAndLoadPayload extends SocketMessagePayload {
-}
