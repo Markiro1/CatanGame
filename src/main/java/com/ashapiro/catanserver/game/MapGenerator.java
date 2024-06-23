550917d9bf3020f5010a11b792dc911d709ce2f9
Markiro1 <65sumo@gmail.com>
2024-06-23 14:47:49 +0300

    - added new logic

--- src/main/java/com/ashapiro/catanserver/game/MapGenerator.java
+++ src/main/java/com/ashapiro/catanserver/game/MapGenerator.java
@@ -1,6 +1,7 @@
 package com.ashapiro.catanserver.game;
 
 import com.ashapiro.catanserver.game.enums.EdgeDirection;
+import com.ashapiro.catanserver.game.enums.HarborType;
 import com.ashapiro.catanserver.game.enums.HexType;
 import com.ashapiro.catanserver.game.enums.VertexDirection;
 import com.ashapiro.catanserver.game.factory.EdgeFactory;
@@ -12,6 +13,7 @@ import com.ashapiro.catanserver.game.model.Hex;
 import com.ashapiro.catanserver.game.model.Vertex;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class MapGenerator {
 
@@ -32,7 +34,7 @@ public class MapGenerator {
         this.hexFactory = new HexFactory();
         this.edgeFactory = new EdgeFactory();
         this.gameMap = new HashMap<>();
-        this.seed = 200;
+        this.seed = new Random().nextInt();
         this.mapRandom = new Random(seed);
     }
 
@@ -92,80 +94,92 @@ public class MapGenerator {
                 Hex hexTL = currentHex.getNeighborHex().get(EdgeDirection.TL);
 
                 if (currentHex.getVertices().containsKey(VertexDirection.N) && currentHex.getVertices().get(VertexDirection.N) == null) {
-                    Vertex vertex = vertexFactory.createVertex();
+                    Vertex vertex = vertexFactory.createVertex(currentHex);
                     currentHex.getVertices().put(VertexDirection.N, vertex);
 
                     if (hexTL != null) {
                         hexTL.getVertices().put(VertexDirection.SE, vertex);
+                        vertex.getHexes().add(hexTL);
                     }
 
                     if (hexTR != null) {
                         hexTR.getVertices().put(VertexDirection.SW, vertex);
+                        vertex.getHexes().add(hexTR);
                     }
                 }
 
                 if (currentHex.getVertices().containsKey(VertexDirection.NE) && currentHex.getVertices().get(VertexDirection.NE) == null) {
-                    Vertex vertex = vertexFactory.createVertex();
+                    Vertex vertex = vertexFactory.createVertex(currentHex);
                     currentHex.getVertices().put(VertexDirection.NE, vertex);
 
                     if (hexTR != null) {
                         hexTR.getVertices().put(VertexDirection.S, vertex);
+                        vertex.getHexes().add(hexTR);
                     }
 
                     if (hexR != null) {
                         hexR.getVertices().put(VertexDirection.NW, vertex);
+                        vertex.getHexes().add(hexR);
                     }
                 }
 
                 if (currentHex.getVertices().containsKey(VertexDirection.SE) && currentHex.getVertices().get(VertexDirection.SE) == null) {
-                    Vertex vertex = vertexFactory.createVertex();
+                    Vertex vertex = vertexFactory.createVertex(currentHex);
                     currentHex.getVertices().put(VertexDirection.SE, vertex);
 
                     if (hexR != null) {
                         hexR.getVertices().put(VertexDirection.SW, vertex);
+                        vertex.getHexes().add(hexR);
                     }
 
                     if (hexDR != null) {
                         hexDR.getVertices().put(VertexDirection.N, vertex);
+                        vertex.getHexes().add(hexDR);
                     }
                 }
 
                 if (currentHex.getVertices().containsKey(VertexDirection.S) && currentHex.getVertices().get(VertexDirection.S) == null) {
-                    Vertex vertex = vertexFactory.createVertex();
+                    Vertex vertex = vertexFactory.createVertex(currentHex);
                     currentHex.getVertices().put(VertexDirection.S, vertex);
 
                     if (hexDR != null) {
                         hexDR.getVertices().put(VertexDirection.NW, vertex);
+                        vertex.getHexes().add(hexDR);
                     }
 
                     if (hexDL != null) {
                         hexDL.getVertices().put(VertexDirection.NE, vertex);
+                        vertex.getHexes().add(hexDL);
                     }
                 }
 
                 if (currentHex.getVertices().containsKey(VertexDirection.SW) && currentHex.getVertices().get(VertexDirection.SW) == null) {
-                    Vertex vertex = vertexFactory.createVertex();
+                    Vertex vertex = vertexFactory.createVertex(currentHex);
                     currentHex.getVertices().put(VertexDirection.SW, vertex);
 
                     if (hexDL != null) {
                         hexDL.getVertices().put(VertexDirection.N, vertex);
+                        vertex.getHexes().add(hexDL);
                     }
 
                     if (hexL != null) {
                         hexL.getVertices().put(VertexDirection.SE, vertex);
+                        vertex.getHexes().add(hexL);
                     }
                 }
 
                 if (currentHex.getVertices().containsKey(VertexDirection.NW) && currentHex.getVertices().get(VertexDirection.NW) == null) {
-                    Vertex vertex = vertexFactory.createVertex();
+                    Vertex vertex = vertexFactory.createVertex(currentHex);
                     currentHex.getVertices().put(VertexDirection.NW, vertex);
 
                     if (hexL != null) {
                         hexL.getVertices().put(VertexDirection.NE, vertex);
+                        vertex.getHexes().add(hexL);
                     }
 
                     if (hexTL != null) {
                         hexTL.getVertices().put(VertexDirection.S, vertex);
+                        vertex.getHexes().add(hexTL);
                     }
                 }
             }
@@ -184,56 +198,62 @@ public class MapGenerator {
                 Hex hexTL = currentHex.getNeighborHex().get(EdgeDirection.TL);
 
                 if (currentHex.getEdges().get(EdgeDirection.TR) == null) {
-                    Edge edge = edgeFactory.createEdge(currentHex.getVertices().get(VertexDirection.N), currentHex.getVertices().get(VertexDirection.NE));
+                    Edge edge = edgeFactory.createEdge(currentHex.getVertices().get(VertexDirection.N), currentHex.getVertices().get(VertexDirection.NE), currentHex);
                     currentHex.getEdges().put(EdgeDirection.TR, edge);
 
                     if (hexTR != null) {
                         hexTR.getEdges().put(EdgeDirection.DL, edge);
+                        edge.getHexes().add(hexTR);
                     }
                 }
 
                 if (currentHex.getEdges().get(EdgeDirection.R) == null) {
-                    Edge edge = edgeFactory.createEdge(currentHex.getVertices().get(VertexDirection.NE), currentHex.getVertices().get(VertexDirection.SE));
+                    Edge edge = edgeFactory.createEdge(currentHex.getVertices().get(VertexDirection.NE), currentHex.getVertices().get(VertexDirection.SE), currentHex);
                     currentHex.getEdges().put(EdgeDirection.R, edge);
 
                     if (hexR != null) {
                         hexR.getEdges().put(EdgeDirection.L, edge);
+                        edge.getHexes().add(hexR);
                     }
                 }
 
                 if (currentHex.getEdges().get(EdgeDirection.DR) == null) {
-                    Edge edge = edgeFactory.createEdge(currentHex.getVertices().get(VertexDirection.SE), currentHex.getVertices().get(VertexDirection.S));
+                    Edge edge = edgeFactory.createEdge(currentHex.getVertices().get(VertexDirection.SE), currentHex.getVertices().get(VertexDirection.S), currentHex);
                     currentHex.getEdges().put(EdgeDirection.DR, edge);
 
                     if (hexDR != null) {
                         hexDR.getEdges().put(EdgeDirection.TL, edge);
+                        edge.getHexes().add(hexDL);
                     }
                 }
 
                 if (currentHex.getEdges().get(EdgeDirection.DL) == null) {
-                    Edge edge = edgeFactory.createEdge(currentHex.getVertices().get(VertexDirection.S), currentHex.getVertices().get(VertexDirection.SW));
+                    Edge edge = edgeFactory.createEdge(currentHex.getVertices().get(VertexDirection.S), currentHex.getVertices().get(VertexDirection.SW), currentHex);
                     currentHex.getEdges().put(EdgeDirection.DL, edge);
 
                     if (hexDL != null) {
                         hexDL.getEdges().put(EdgeDirection.TR, edge);
+                        edge.getHexes().add(hexDL);
                     }
                 }
 
                 if (currentHex.getEdges().get(EdgeDirection.L) == null) {
-                    Edge edge = edgeFactory.createEdge(currentHex.getVertices().get(VertexDirection.SW), currentHex.getVertices().get(VertexDirection.NW));
+                    Edge edge = edgeFactory.createEdge(currentHex.getVertices().get(VertexDirection.SW), currentHex.getVertices().get(VertexDirection.NW), currentHex);
                     currentHex.getEdges().put(EdgeDirection.L, edge);
 
                     if (hexL != null) {
                         hexL.getEdges().put(EdgeDirection.R, edge);
+                        edge.getHexes().add(hexL);
                     }
                 }
 
                 if (currentHex.getEdges().get(EdgeDirection.TL) == null) {
-                    Edge edge = edgeFactory.createEdge(currentHex.getVertices().get(VertexDirection.NW), currentHex.getVertices().get(VertexDirection.N));
+                    Edge edge = edgeFactory.createEdge(currentHex.getVertices().get(VertexDirection.NW), currentHex.getVertices().get(VertexDirection.N), currentHex);
                     currentHex.getEdges().put(EdgeDirection.TL, edge);
 
                     if (hexTL != null) {
                         hexTL.getEdges().put(EdgeDirection.DR, edge);
+                        edge.getHexes().add(hexTL);
                     }
                 }
             }
@@ -250,6 +270,7 @@ public class MapGenerator {
         List<Hex> hexes = hexFactory.getHexes();
         setHexesType(hexes, hexTypes);
         generateHexesNumberTokens(hexes);
+        generateHarbors();
 
         return gameMap;
     }
@@ -320,5 +341,63 @@ public class MapGenerator {
             }
             hexes.get(i).setNumberToken(numberTokensQueue.poll());
         }
+    }
+
+    private void generateHarbors() {
+        List<Edge> boundaryEdges = getEdges().stream()
+                .filter(edge -> edge.getHexes().size() == 1)
+                .collect(Collectors.toList());
+        List<Edge> boundaryEdgesInOrder = getBoundaryEdgesInRightOrder(boundaryEdges);
+
+        List<Integer> spaceBetweenHarbors = new ArrayList<>(List.of(3, 3, 4));
+        List<HarborType> harborTypes = new ArrayList<>(List.of(
+                HarborType.ORE,
+                HarborType.BRICK,
+                HarborType.LUMBER,
+                HarborType.GRAIN,
+                HarborType.WOOL,
+                HarborType.GENERIC,
+                HarborType.GENERIC,
+                HarborType.GENERIC,
+                HarborType.GENERIC
+        ));
+        shuffle(harborTypes);
+        int startIndexEdge = mapRandom.nextInt(boundaryEdgesInOrder.size());
+        int finalIndexEdge = startIndexEdge + boundaryEdges.size();
+        int index = startIndexEdge;
+
+        int i = 0;
+        while (index < finalIndexEdge) {
+            List<Vertex> vertices = boundaryEdgesInOrder.get(index % boundaryEdgesInOrder.size()).getNeighborVertices();
+            HarborType harborType = harborTypes.get(i % harborTypes.size());
+            index += spaceBetweenHarbors.get(i % spaceBetweenHarbors.size());
+            i++;
+
+            for (Vertex vertex : vertices) {
+                vertex.setHarborType(harborType);
+            }
+        }
+    }
+
+    private List<Edge> getBoundaryEdgesInRightOrder(List<Edge> boundaryEdges) {
+        List<Edge> boundaryEdgesInOrder = new ArrayList<>();
+        Edge currentEdge = boundaryEdges.get(0);
+        boundaryEdgesInOrder.add(currentEdge);
+        boolean isFound = true;
+
+        while (isFound) {
+            isFound = false;
+            currentEdge = MapUtil.getNeighboursEdgeToEdge(currentEdge).stream()
+                    .filter(edge -> boundaryEdges.contains(edge) && !boundaryEdgesInOrder.contains(edge))
+                    .findFirst()
+                    .orElse(null);
+
+            if (currentEdge != null) {
+                isFound = true;
+                boundaryEdgesInOrder.add(currentEdge);
+            }
+        }
+
+        return new ArrayList<>(boundaryEdgesInOrder);
     }
 }
