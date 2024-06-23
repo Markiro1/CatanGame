3d26d7d89ca3c30d15d1ac937ec11b0cfab45d14
Markiro1 <65sumo@gmail.com>
2024-06-23 14:48:23 +0300

    - added new messages

--- src/main/java/com/ashapiro/catanserver/socketServer/util/MessageRegistry.java
+++ src/main/java/com/ashapiro/catanserver/socketServer/util/MessageRegistry.java
@@ -13,15 +13,23 @@ public class MessageRegistry {
     static {
         registry.put(EventType.REQUEST_CONNECT, SocketRequestConnectPayload.class);
         registry.put(EventType.REQUEST_START_GAME, SocketRequestStartGamePayload.class);
-        registry.put(EventType.REQUEST_READY_AND_LOAD, SocketRequestReadyAndLoadPayload.class);
         registry.put(EventType.REQUEST_BUILD_SETTLEMENT, SocketRequestBuildPayload.class);
         registry.put(EventType.REQUEST_BUILD_ROAD, SocketRequestBuildPayload.class);
         registry.put(EventType.REQUEST_BUILD_CITY, SocketRequestBuildPayload.class);
-        registry.put(EventType.REQUEST_USER_TURN_READY, SocketRequestUserTurnReadyPayload.class);
         registry.put(EventType.REQUEST_TRADE_RESOURCE, SocketRequestTradeResourcePayload.class);
+        registry.put(EventType.REQUEST_USER_ROBBERY, SocketRequestRobberyPayload.class);
+        registry.put(EventType.REQUEST_USE_KNIGHT_CARD, SocketRequestUseKnightCardPayload.class);
+        registry.put(EventType.REQUEST_USE_MONOPOLY_CARD, SocketRequestUseMonopolyCardPayload.class);
+        registry.put(EventType.REQUEST_USE_YEAR_OF_PLENTY_CARD, SocketRequestUseYearOfPlentyCardPayload.class);
+        registry.put(EventType.REQUEST_READY_AND_LOAD, DefaultSocketMessagePayload.class);
+        registry.put(EventType.REQUEST_USER_TURN_READY, DefaultSocketMessagePayload.class);
+        registry.put(EventType.REQUEST_BUY_CARD, DefaultSocketMessagePayload.class);
+        registry.put(EventType.REQUEST_USE_ROAD_BUILDING_CARD, DefaultSocketMessagePayload.class);
+        registry.put(EventType.REQUEST_EXCHANGE_OFFER, SocketRequestExchangeResourcesOfferPayload.class);
+        registry.put(EventType.REQUEST_EXCHANGE, SocketRequestExchangePayload.class);
     }
 
-    public static <T extends SocketMessagePayload> Class<T> getMessageClass(EventType eventType) {
-        return (Class<T>) registry.get(eventType);
+    public static Class<SocketMessagePayload> getMessageClass(EventType eventType) {
+        return (Class<SocketMessagePayload>) registry.get(eventType);
     }
 }
