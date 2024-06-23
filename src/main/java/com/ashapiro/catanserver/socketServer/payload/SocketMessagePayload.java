a6ef1b02dd7d3ce5a6e36815800ff8d4bc94f3e7
Markiro1 <65sumo@gmail.com>
2024-06-23 14:50:36 +0300

    - change from class to interface

--- src/main/java/com/ashapiro/catanserver/socketServer/payload/SocketMessagePayload.java
+++ src/main/java/com/ashapiro/catanserver/socketServer/payload/SocketMessagePayload.java
@@ -1,16 +1,7 @@
 package com.ashapiro.catanserver.socketServer.payload;
 
 import com.ashapiro.catanserver.enums.EventType;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.experimental.SuperBuilder;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter @Setter
-@SuperBuilder
-public class SocketMessagePayload {
-    private EventType eventType;
+public interface SocketMessagePayload {
+    EventType getEventType();
 }
