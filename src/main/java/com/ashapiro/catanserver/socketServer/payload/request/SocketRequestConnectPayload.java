056a33fa0cbac807a401c5b7fe71f75c0efd728f
Markiro1 <65sumo@gmail.com>
2024-06-23 14:51:49 +0300

    - refactor socketPayload classes

--- src/main/java/com/ashapiro/catanserver/socketServer/payload/request/SocketRequestConnectPayload.java
+++ src/main/java/com/ashapiro/catanserver/socketServer/payload/request/SocketRequestConnectPayload.java
@@ -1,16 +1,17 @@
 package com.ashapiro.catanserver.socketServer.payload.request;
 
+import com.ashapiro.catanserver.enums.EventType;
 import com.ashapiro.catanserver.socketServer.payload.SocketMessagePayload;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.experimental.SuperBuilder;
 
-@AllArgsConstructor
 @NoArgsConstructor
-@Getter @Setter
-@SuperBuilder
-public class SocketRequestConnectPayload extends SocketMessagePayload {
+@AllArgsConstructor
+@Getter
+public class SocketRequestConnectPayload implements SocketMessagePayload {
+
+    private EventType eventType;
+
     private String token;
 }
